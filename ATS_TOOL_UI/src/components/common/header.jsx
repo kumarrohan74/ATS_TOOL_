9b82d3415a9a13e3d324a9984ca7a91f5dddb702
Kumar Rohan <kumar.rohan74@gmail.com>
2024-10-05 19:52:59 +0530

    Changed the color combination of header, sidebar

--- ATS_TOOL_UI/src/components/common/header.jsx
+++ ATS_TOOL_UI/src/components/common/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="h-[60px] bg-sky-500">
+        <div className="h-[60px] cursor-pointer bg-indigo-600 hover:bg-indigo-500">
             <h1 className="text-white text-4xl p-2">
                 ATS
             </h1>
--- ATS_TOOL_UI/src/components/common/sidebar.jsx
+++ ATS_TOOL_UI/src/components/common/sidebar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Sidebar = () => {
     return (
-        <div className="w-[250px] border-r-2 border-sky-500 h-screen ">
+        <div className="w-[250px] border-0 shadow-lg shadow-right h-screen bg-gray-100">
           <ul>
-            <li className="p-4 pl-0 hover:bg-gray-200 hover:text-stone-700 cursor-pointer text-center">Candidate list</li>
-            <li className='mb-3 p-4 pl-0 text-center text-base hover:bg-gray-200 hover:text-stone-700 cursor-pointer'>Add profile</li>
+            <li className="p-4 pl-0 cursor-pointer text-center text-gray-700 font-bold hover:bg-indigo-100 hover:text-indigo-600">Candidate list</li>
+            <li className='mb-3 p-4 pl-0 text-center text-gray-700  text-base cursor-pointer font-bold hover:bg-indigo-100 hover:text-indigo-600'>Add profile</li>
           </ul>
             
         </div>
