db9f984589dfdf732aaa09e34e679c5ed71d8476
Deepankar <deepankar0601@gmail.com>
2024-10-10 17:18:43 +0530

    add addProfile component, Table Loader and Modal component feature/ATS_12

--- ATS_TOOL_UI/src/components/Candidatelist.jsx
+++ ATS_TOOL_UI/src/components/Candidatelist.jsx
@@ -31,27 +31,17 @@ const columns = [
     },
 ];
 
-/*const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];*/
+
 
 function Candidatelist() {
-    const data = React.useContext(CandidateContext);
+    const { data } = React.useContext(CandidateContext);
 
     const candidates = data.candidates;
     let rows;
     if (candidates) {
 
-        rows = candidates.map((details) => {
-            return { id: details.id, name: details.name, email: details.email, score: details.ats_score, position: details.position }
+        rows = candidates.map((details, i) => {
+            return { id: i + 1, name: details.name, email: details.email, score: details.ats_score, position: details.position }
         })
     }
     return (
--- ATS_TOOL_UI/src/components/Context.js
+++ ATS_TOOL_UI/src/components/Context.js
@@ -5,17 +5,19 @@ export const CandidateContext = createContext();
 export const DataProvider = ({ children }) => {
     const [data, setData] = useState('');
     const apiURI = process.env.REACT_APP_API_URL;
-
+    const [loader, setLoader] = useState(true)
     useEffect(() => {
         fetch(`${apiURI}/get-candidates`)
             .then(res => res.json())
             .then(response => {
                 setData(response);
+                setLoader(false);
+
             })
             .catch(err => console.error(err))
     }, [apiURI]);
     return (
-        <CandidateContext.Provider value={data}>
+        <CandidateContext.Provider value={{ data, loader }}>
             {children}
         </CandidateContext.Provider>)
 
