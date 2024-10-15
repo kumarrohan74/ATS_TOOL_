4f1cdf35051d71bb61843c9103b9983307ca5db6
Kumar Rohan <kumar.rohan74@gmail.com>
2024-10-15 21:53:44 +0530

    Add automatically logout feature

--- ATS_TOOL_UI/src/components/common/sidebar.jsx
+++ ATS_TOOL_UI/src/components/common/sidebar.jsx
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 
 
 const Sidebar = () => {
+
   const location = useLocation();
   const navigate = useNavigate();
+
   const [activePath, setActivePath] = useState(location.pathname)
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActivePath('/candidatelist')
+    }
+    else {
+      setActivePath(location.pathname)
+    }
+  }, [])
+
   const handleClick = (e) => {
-    if(e.target.id==='candidatelist'){
+    if (e.target.id === 'candidatelist') {
       navigate('/candidatelist')
       setActivePath('/candidatelist')
-      }
-      if(e.target.id==='addProfile'){
-        navigate('/addprofile')
+    }
+    if (e.target.id === 'addProfile') {
+      navigate('/addprofile')
       setActivePath('/addprofile')
-      }
-   
+    }
   }
 
   return (
     <div className="w-[250px] relative border-0 shadow-lg shadow-right h-screen bg-gray-100 flex-shrink-0">
       <ul>
-        <li className={` p-4 pl-0 cursor-pointer text-center text-gray-700 font-bold hover:bg-indigo-100 hover:text-indigo-600 ${activePath === '/candidatelist' ? 'text-indigo-600 bg-indigo-100' : ''}`} id='candidatelist' onClick={(e)=>handleClick(e)}>Candidate list</li>
-        <li className={`mb-3 p-4 pl-0 text-center text-gray-700  text-base cursor-pointer font-bold hover:bg-indigo-100 hover:text-indigo-600 ${activePath === '/addprofile' ? 'text-indigo-600 bg-indigo-100' : ''}`} id='addProfile' onClick={(e)=>handleClick(e)}>Add profile</li>
+        <li className={` p-4 pl-0 cursor-pointer text-center text-gray-700 font-bold hover:bg-indigo-100 hover:text-indigo-600 ${activePath === '/candidatelist' ? 'text-indigo-600 bg-indigo-100' : ''}`} id='candidatelist' onClick={(e) => handleClick(e)}>Candidate list</li>
+        <li className={`mb-3 p-4 pl-0 text-center text-gray-700  text-base cursor-pointer font-bold hover:bg-indigo-100 hover:text-indigo-600 ${activePath === '/addprofile' ? 'text-indigo-600 bg-indigo-100' : ''}`} id='addProfile' onClick={(e) => handleClick(e)}>Add profile</li>
       </ul>
 
     </div>
--- ATS_TOOL_UI/src/components/routes.jsx
+++ ATS_TOOL_UI/src/components/routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Candidatelist from './Candidatelist';
 import AddProfile from './AddProfile';
 import CandidateProfile from './CandidateProfile';
@@ -6,6 +6,7 @@ export function Router() {
 
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/candidatelist" />} />
             <Route path='/candidatelist' element={<Candidatelist />} />
             <Route path='/candidate/:id' element={<CandidateProfile />} />
             <Route path='/addprofile' element={<AddProfile />} />
