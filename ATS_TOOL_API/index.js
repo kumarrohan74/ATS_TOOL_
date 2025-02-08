c67fd3effbe2d69f40d304ffde5733f9a3d1369f
Kumar Rohan <kumar.rohan74@gmail.com>
2025-02-08 15:21:38 +0530

    CORS

--- ATS_TOOL_API/index.js
+++ ATS_TOOL_API/index.js
@@ -37,7 +37,7 @@ app.use(cors({
 app.options('*', cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ats.aminobots.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
